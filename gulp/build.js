338eb92e6798f886959e4ecb7a46f555da29fce6
Zachary Iles <zack.iles@gmail.com>
2015-08-17 05:29:00 -0400

    fx

--- gulp/build.js
+++ gulp/build.js
@@ -164,7 +164,7 @@ gulp.task('other', function () {
   var icon = gulp.src(conf.paths.src + 'favicon.ico').pipe(gulp.dest('dist/'));
   var robots = gulp.src(conf.paths.src + 'robots.txt').pipe(gulp.dest('dist/'));
 
-  return $.eventStream.concat(icon, robots);
+  return es.merge(icon, robots);
 });
 
 
