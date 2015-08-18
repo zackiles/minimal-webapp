5aa4bbf8794de5775d42dd951ef7ac6e13fbab1d
Zachary Iles <zack.iles@gmail.com>
2015-08-17 22:52:51 -0400

    update gulp conf with proxy options

--- gulp/conf.js
+++ gulp/conf.js
@@ -1,38 +1,42 @@
 'use strict';
 
-var gulp = require('gulp'),
-		gutil = require('gulp-util'),
+var gutil = require('gulp-util'),
     path = require('path');
 
-// keep backslashes
-var paths = {
-  src: 'src/',
-  dist: 'dist/',
-  tmp: '.tmp/'
-};
-
-exports.paths = paths;
-
-exports.pipes = {
-	scripts: function(config){
-		return gulp.src([
-			paths.src + '{app,components}/**/*.js',
-			'!' + paths.src + '{app,components}/**/*.spec.js',
-			'!' + paths.src + '{app,components}/**/*.mock.js'
-		], config || {});
+module.exports = {
+  server:  {
+		port: 9090,
+		proxy: {
+			path: '/api',
+			location: 'http://localhost:3000/api'
+		}
 	},
-	scss: function(config){
-		return gulp.src([
-			paths.src + 'assets/styles/**/*.scss',
-			paths.src + '{app,components}/**/*.scss',
-			'!' + paths.src + 'app/app.scss'
-		], config || {});
-	}
-};
-
-exports.errorHandler = function(title) {
-  return function(err) {
-    gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
-    this.emit('end');
-  };
+  paths: {
+		// KEEP DIR BACKSLASHES
+		src: 'src/',
+		dist: 'dist/',
+		tmp: '.tmp/'
+	},
+	pipes: {
+		scripts: function(config){
+			return gulp.src([
+				paths.src + '{app,components}/**/*.js',
+				'!' + paths.src + '{app,components}/**/*.spec.js',
+				'!' + paths.src + '{app,components}/**/*.mock.js'
+			], config || {});
+		},
+		scss: function(config){
+			return gulp.src([
+				paths.src + 'assets/styles/**/*.scss',
+				paths.src + '{app,components}/**/*.scss',
+				'!' + paths.src + 'app/app.scss'
+			], config || {});
+		}
+	},
+  errorHandler: function(title) {
+    return function(err) {
+      gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+      this.emit('end');
+    };
+  }
 };
